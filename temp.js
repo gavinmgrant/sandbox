e0e2345a60ab85956dfac7c073cef94840475efd
Gavin Grant <gavingrant@gmail.com>
2020-12-01 21:04:35 -0800

    feat(fizzbuzz): add problem and solution

--- temp.js
+++ temp.js
@@ -1,13 +1,21 @@
 // store temporary info here - delete when done
 
-const countingSheep = function(sheep) {
-    // base case
-    if (sheep === 0) {
-        return 'All sheep jumped over the fence';
-    }
+const findMode = arr => {
+    if (!arr) return null;
+    let numCount = {};
 
-    // general case
-    return sheep + ': Another sheep jumps over the fence\n' + (countingSheep(sheep - 1));
-};
+    arr.forEach(num => {
+        if (!numCount[num]) {
+            numCount[num] = 1;
+        } else {
+            numCount[num]++;
+        }
+    })
 
-console.log(countingSheep(6));
+    const mode = Object.keys(numCount).reduce((a, b) => numCount[a] > numCount[b] ? a : b);
+    const count = Math.max(...Object.values(numCount));
+
+    return `mode= ${mode} count= ${count}`;
+}
+
+console.log(findMode([1, 2, 3, 6, 10, 3, 5, 6, 3, 3]))
